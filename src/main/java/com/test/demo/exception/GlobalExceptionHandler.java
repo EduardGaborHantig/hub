8a02fecfaed03b3ac01d1eb3c35b5032722b2d7c
Hantig Eduard-Gabor <h.eduardgabor@gmail.com>
2024-08-06 20:03:59 +0200

    Add global exception handler

---
+++ src/main/java/com/test/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.test.demo.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ExceptionResponseModel> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getFieldErrors()
+        .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+    return new ResponseEntity<>(new ExceptionResponseModel(errors, HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<ExceptionResponseModel> handleIllegalArgumentException(IllegalArgumentException ex) {
+    return new ResponseEntity<>(new ExceptionResponseModel(ex.getMessage(), HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(ProductNotFoundException.class)
+  public ResponseEntity<ExceptionResponseModel> handleProductNotFoundException(ProductNotFoundException ex) {
+    return new ResponseEntity<>(new ExceptionResponseModel(ex.getMessage(), HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ExceptionResponseModel> handleGlobalException(Exception ex) {
+    return new ResponseEntity<>(new ExceptionResponseModel("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
