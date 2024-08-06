da832e6c85d90c80602bdf2d549d48385c778f55
Hantig Eduard-Gabor <h.eduardgabor@gmail.com>
2024-08-06 20:25:49 +0200

    Update to use records for Exception response model

--- src/main/java/com/test/demo/exception/ExceptionResponseModel.java
+++ src/main/java/com/test/demo/exception/ExceptionResponseModel.java
@@ -1,16 +1,5 @@
 package com.test.demo.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class ExceptionResponseModel {
-
-  private Object errors;
-
-  private HttpStatus code;
-}
+public record ExceptionResponseModel(Object errors, HttpStatus code) { }
