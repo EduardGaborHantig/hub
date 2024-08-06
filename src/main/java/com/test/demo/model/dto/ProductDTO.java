d82e60ae8ac93155287f15ec159f045aeb15cf52
Hantig Eduard-Gabor <h.eduardgabor@gmail.com>
2024-08-06 20:22:27 +0200

    Update to use records for DTO

--- src/main/java/com/test/demo/model/dto/ProductDTO.java
+++ src/main/java/com/test/demo/model/dto/ProductDTO.java
@@ -3,24 +3,6 @@ package com.test.demo.model.dto;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class ProductDTO {
-
-  private String id;
-
-  @NotEmpty(message = "Name is mandatory")
-  private String name;
-
-  private Integer stock;
-
-  @Positive(message = "Price must be a positive value")
-  private Double price;
-
-  private LocalDateTime createdDate;
-}
+public record ProductDTO(String id, @NotEmpty(message = "Name is mandatory") String name, Integer stock,
+                         @Positive(message = "Price must be a positive value") Double price, LocalDateTime createdDate) { }
