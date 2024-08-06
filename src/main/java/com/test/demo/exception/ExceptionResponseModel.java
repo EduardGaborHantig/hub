5c32042c0328072bdc1091d45b568075797b02d8
Hantig Eduard-Gabor <h.eduardgabor@gmail.com>
2024-08-06 20:03:14 +0200

    Add exception model to be used for propagating exceptions

---
+++ src/main/java/com/test/demo/exception/ExceptionResponseModel.java
@@ -0,0 +1,16 @@
+package com.test.demo.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponseModel {
+
+  private Object errors;
+
+  private HttpStatus code;
+}
