ace4235a1f7aa2967a00fd692c4cd43f9918e3e1
Hantig Eduard-Gabor <h.eduardgabor@gmail.com>
2024-08-06 20:19:00 +0200

    Add api documentation with swagger

--- src/main/java/com/test/demo/controller/ProductController.java
+++ src/main/java/com/test/demo/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.test.demo.controller;
 
 import com.test.demo.model.dto.ProductDTO;
 import com.test.demo.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -22,38 +24,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/products")
+@Tag(name = "Products", description = "API for managing products")
 public class ProductController {
 
   private final ProductService productService;
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
+  @Operation(summary = "Create a new product", description = "Create a new product")
   public ProductDTO createProduct(@Valid @RequestBody ProductDTO productDTO) {
     return productService.createProduct(productDTO);
   }
 
   @PutMapping("/{id}")
+  @Operation(summary = "Update a product", description = "Update an existing product")
   public ProductDTO updateProduct(@PathVariable String id, @Valid @RequestBody ProductDTO productDTO) {
     return productService.updateProduct(id, productDTO);
   }
 
   @PatchMapping("/{id}/price")
+  @Operation(summary = "Update the price of a product", description = "Update the price of a product")
   public ProductDTO updateProductPrice(@PathVariable String id, @RequestParam @Positive(message = "Price must be a positive value") Double price) {
     return productService.updateProductPrice(id, price);
   }
 
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
+  @Operation(summary = "Delete a product", description = "Delete a product by its ID")
   public void deleteProduct(@PathVariable String id) {
     productService.deleteProduct(id);
   }
 
   @GetMapping("/{id}")
+  @Operation(summary = "Get a product by ID", description = "Retrieve a product by its ID")
   public ProductDTO getProductById(@PathVariable String id) {
     return productService.getProductById(id);
   }
 
   @GetMapping
+  @Operation(summary = "Get all products", description = "Retrieve all products")
   public Page<ProductDTO> getAllProducts(
       @RequestParam(defaultValue = "0") int page,
       @RequestParam(defaultValue = "10") int size) {
