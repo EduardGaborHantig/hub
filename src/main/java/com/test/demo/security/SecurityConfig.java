609c17d7b449ab0f5a01755dda930d31330cc6f9
Hantig Eduard-Gabor <h.eduardgabor@gmail.com>
2024-08-07 08:46:22 +0200

    Extract credentials in properties

--- src/main/java/com/test/demo/security/SecurityConfig.java
+++ src/main/java/com/test/demo/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.test.demo.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Value("${hub.user.role.username}")
+  private String userUsername;
+
+  @Value("${hub.user.role.password}")
+  private String userPassword;
+
+  @Value("${hub.admin.role.username}")
+  private String adminUsername;
+
+  @Value("${hub.admin.role.password}")
+  private String adminPassword;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
@@ -27,14 +40,14 @@ public class SecurityConfig {
   @Bean
   public UserDetailsService userDetailsService() {
     UserDetails user = User.withDefaultPasswordEncoder()
-        .username("user")
-        .password("password")
+        .username(userUsername)
+        .password(userPassword)
         .roles("USER")
         .build();
 
     UserDetails admin = User.withDefaultPasswordEncoder()
-        .username("admin")
-        .password("admin")
+        .username(adminUsername)
+        .password(adminPassword)
         .roles("ADMIN")
         .build();
 
