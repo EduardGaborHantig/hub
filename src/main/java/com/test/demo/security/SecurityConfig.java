4597faf6714d9e35e4c3cf76a9a2c77f1c826316
Hantig Eduard-Gabor <h.eduardgabor@gmail.com>
2024-08-07 10:34:18 +0200

    Fix security by addding httpBasic and encoder

--- src/main/java/com/test/demo/security/SecurityConfig.java
+++ src/main/java/com/test/demo/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.test.demo.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -32,25 +36,33 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+        .authorizeHttpRequests(requests -> requests
+            .requestMatchers("/api/v1/products/**").hasRole("ADMIN")
+            .anyRequest().authenticated())
+        .httpBasic(withDefaults());
     return http.build();
 
   }
 
   @Bean
   public UserDetailsService userDetailsService() {
-    UserDetails user = User.withDefaultPasswordEncoder()
+    UserDetails user = User.builder()
         .username(userUsername)
-        .password(userPassword)
+        .password(passwordEncoder().encode(userPassword))
         .roles("USER")
         .build();
 
-    UserDetails admin = User.withDefaultPasswordEncoder()
+    UserDetails admin = User.builder()
         .username(adminUsername)
-        .password(adminPassword)
+        .password(passwordEncoder().encode(adminPassword))
         .roles("ADMIN")
         .build();
 
     return new InMemoryUserDetailsManager(user, admin);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
